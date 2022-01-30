d33989a2daf584b582ef3c7d50bd7273e26295f3
NagatsumaHiroki <act1911@gmail.com>
2022-01-30 14:27:27 +0900

    サンプルデータ表示 -画面作成 -Model作成 -SQL作成 -Controller作成

---
+++ web/src/Controller/PostsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller;
+
+class PostsController extends AppController
+{
+    public function index()
+    {
+        $this->viewBuilder()->layout('my_layout');
+        $posts = $this->Posts->find('all')->order(['title' => 'DESC']);
+        $this->set(compact('posts'));
+    }
+}
