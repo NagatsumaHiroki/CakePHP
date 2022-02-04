beb0bcd3953d0d0b36dd6eaccba348b04b724c20
NagatsumaHiroki <act1911@gmail.com>
2022-02-05 08:03:30 +0900

    Flash処理追加 バリデーションメッセージレイアウト修正

--- web/src/Controller/PostsController.php
+++ web/src/Controller/PostsController.php
@@ -6,6 +6,7 @@ class PostsController extends AppController
 {
     public function initialize()
     {
+        $this->loadComponent('Flash');
         $this->viewBuilder()->layout('my_layout');
     }
     public function index()
@@ -23,6 +24,17 @@ class PostsController extends AppController
     public function add()
     {
         $post = $this->Posts->newEntity();
+        //Post送信のデータか判定
+        if ($this->request->is('post')) {
+            $post = $this->Posts->patchEntity($post, $this->request->data());
+            if ($this->Posts->save($post)) {
+                $this->Flash->success('Add Success!');
+                return $this->redirect(['action' => 'index']);
+            } else {
+                //error
+                $this->Flash->error('Add Error!');
+            };
+        }
         $this->set(compact('post'));
     }
 }
